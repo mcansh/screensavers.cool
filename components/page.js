6e745517500eb10813fcbc45ad9ce0dec3ff3b47
Logan McAnsh <logan@mcan.sh>
2017-08-26 15:44:11 -0500

    implicit return

--- components/page.js
+++ components/page.js
@@ -2,16 +2,14 @@ import Header from '../components/header'
 import Footer from '../components/footer'
 import Section from '../components/section'
 
-const Page = (props) => {
-  return(
-    <div>
-      <Header />
-      <Section>
-        {props.children}
-      </Section>
-      <Footer />
-    </div>
-  )
-}
+const Page = (props) => (
+  <div>
+    <Header />
+    <Section>
+      {props.children}
+    </Section>
+    <Footer />
+  </div>
+);
 
 export default Page
